9214437f52db78a7aa44c21cb8d62c88c2ba1ce4
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-07-30 22:16:13 +0900

    feat: 구글 폼 링크 추가

--- components/ui/apply-button.tsx
+++ components/ui/apply-button.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 interface ApplyButtonProps {
   className?: string;
@@ -9,9 +10,9 @@ interface ApplyButtonProps {
 }
 
 export function ApplyButton({ className = "", size = "lg" }: ApplyButtonProps) {
-  const handleApplyClick = () => {
-    alert("지금은 모집 중이 아닙니다!");
-  };
+  // const handleApplyClick = () => {
+  //   alert("지금은 모집 중이 아닙니다!");
+  // };
 
   const sizeClasses = {
     sm: "px-2 py-1.5 text-xs",
@@ -27,13 +28,16 @@ export function ApplyButton({ className = "", size = "lg" }: ApplyButtonProps) {
 
   return (
     <>
-      <Button
-        onClick={handleApplyClick}
-        className={`bg-blue-500 hover:bg-blue-600 text-white rounded-full font-medium ${sizeClasses[size]} ${className}`}
-      >
-        10기 알림받기
-        <ArrowRight className={`${iconSizes[size]}`} />
-      </Button>
+      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfHFzTwviIJ7XCqwK8OyCwP5-WJIj_sgcPLP8xyqTZrCl_WPA/viewform?usp=dialog">
+        <Button
+          // onClick={handleApplyClick}
+          className={`bg-blue-500 hover:bg-blue-600 text-white rounded-full font-medium ${sizeClasses[size]} ${className}`}
+        >
+          10기 알림받기
+          <ArrowRight className={`${iconSizes[size]}`} />
+        </Button>
+      </Link>
+
       {/* <Link href="/">
             <Button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full px-6 py-2 text-sm font-medium">
               10기 지원하기
